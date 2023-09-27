12bbc9a2f5de44f2153bf82b126321c45a98d8e0
Julio C. Mesquita <juliocesarmcamilo@gmail.com>
2023-09-26 22:48:20 -0300

    WIP - INIT THE REPOSITORY

---
+++ project-core-template-api-spec/src/test/java/br/com/juliomesquita/projectcoretemplateapispec/ProjectCoreTemplateApiSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliomesquita.projectcoretemplateapispec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCoreTemplateApiSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
